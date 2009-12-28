f1680627a18d8c8ca6c24fe857c4b11111dbb538
Miki Tebeka <miki.tebeka@gmail.com>
2009-12-28 13:03:19 -0800

    Testing on sauce

---
+++ test-sauce-on-sauce.clj
@@ -0,0 +1,70 @@
+; Copyright 2009 Sauce Labs Inc
+; 
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+; 
+;    http://www.apache.org/licenses/LICENSE-2.0
+; 
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS,
+; WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+; See the License for the specific language governing permissions and
+; limitations under the License.
+
+(require 'selenium)
+
+(def test-google
+  {
+   :name "google"
+   :test (fn [client]
+           (doto client
+             (.open "http://www.google.com")
+             (.type "q" "Sauce Labs")
+             (.click "btnG")
+             (.waitForPageToLoad "5000"))
+           (.isTextPresent client "Selenium"))
+   })
+
+(def test-yahoo
+  {
+   :name "sauce"
+   :test (fn [client]
+           (doto client
+             (.open "http://yahoo.com")
+             (.type "p" "Sauce Labs")
+             (.click "search-submit")
+             (.waitForPageToLoad "5000"))
+           (.isTextPresent client "Selenium"))
+   })
+
+
+(def test-bing
+  {
+   :name "bing"
+   :test (fn [client]
+           (doto client
+             (.open "http://www.bing.com")
+             (.type "q" "Sauce Labs")
+             (.click "go")
+             (.waitForPageToLoad "5000"))
+           (.isTextPresent client "Selenium"))
+   })
+
+(def tests [test-google test-yahoo test-bing])
+
+; Our fancy reporter :)
+(defn reporter
+  [results]
+  (doseq [r results]
+    (println r)))
+
+(run-tests tests reporter
+           :host "saucelabs.com"
+           :command "{
+              \"username\" : \"SAUCE-USER-NAME\",
+              \"access-key\" : \"SAUCE-ACCESS-KEY\",
+              \"os\" : \"Windows 2003\",
+              \"browser\" : \"firefox\",
+              \"browser-version\" : \"\"}"
+           :url "http://saucelabs.com")
